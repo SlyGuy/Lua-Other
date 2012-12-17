d398524aac4009cdccc949e2db4fa652634da2a5
zdroid9770 <@zdroid9770@yahoo.com>
2012-12-16 17:26:50 -0800

    r0100: Missing Bosses, Bug fixes, Cata & Mists Dungeons

---
+++ Blizzlike/ArcEmu/Scripts/Instances/Classic/Dungeons/Dire Maul/East/Hydrospawn.lua
@@ -0,0 +1,4 @@
+--[[Massive Geyser - Summons a massive geyser, knocking a target high into the air.
+Riptide  — Knocks back anyone within a 10 yard radius, dealing 85 to 115 Frost damage. Also disarms all targets hit for 4 sec.
+Submersion  — Reduces the Stamina of nearby enemies by 10 for 1 min.
+Summon Hydroling - Summons a Hydroling to assist the caster.]]--
---
+++ Blizzlike/ArcEmu/Scripts/Instances/Classic/Dungeons/Dire Maul/West/Tendris Warpwood.lua
@@ -0,0 +1,8 @@
+--[[Grasping Vines  — Inflicts Physical damage to nearby enemies, knocking them down and immobilizing them for 10 sec.
+Entangle  — Entangles an enemy in roots, inflicting Nature damage every 3 sec. and immobilizing it for up to 10 sec.
+
+Quotes
+	Aggro
+		Tendris Warpwood yells: You do not belong here! Ancients, rise up against these intruders!
+	Death
+		Emote: Ancient Equine Spirit breaks free of its spectral bonds with a tremendous crash of thunder!]]--
