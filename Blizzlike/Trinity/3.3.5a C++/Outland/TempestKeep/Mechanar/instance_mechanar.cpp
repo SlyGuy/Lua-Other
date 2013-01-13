110554214ea54c815f191b5e48ad777d9dd84446
zdroid9770 <@zdroid9770@yahoo.com>
2013-01-13 10:35:58 -0600

    027 Updates

--- Blizzlike/Trinity/3.3.5a C++/Outland/TempestKeep/Mechanar/instance_mechanar.cpp
+++ Blizzlike/Trinity/3.3.5a C++/Outland/TempestKeep/Mechanar/instance_mechanar.cpp
@@ -16,52 +16,59 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ScriptData
-SDName: Instance_Mechanar
-SD%Complete: 100
-SDComment:
-SDCategory: Mechanar
-EndScriptData */
 
 #include "ScriptMgr.h"
 #include "InstanceScript.h"
 #include "mechanar.h"
 
+static DoorData const doorData[] =
+{
+    { GO_DOOR_MOARG_1,          DATA_GATEWATCHER_IRON_HAND,     DOOR_TYPE_PASSAGE,  BOUNDARY_NONE },
+    { GO_DOOR_MOARG_2,          DATA_GATEWATCHER_GYROKILL,      DOOR_TYPE_PASSAGE,  BOUNDARY_NONE },
+    { GO_DOOR_NETHERMANCER,     DATA_NETHERMANCER_SEPRETHREA,   DOOR_TYPE_ROOM,     BOUNDARY_NONE },
+    {0,                         0,                              DOOR_TYPE_ROOM,     BOUNDARY_NONE }
+};
+
 class instance_mechanar : public InstanceMapScript
 {
-    public: instance_mechanar(): InstanceMapScript("instance_mechanar", 554) {}
-
-        InstanceScript* GetInstanceScript(InstanceMap* map) const
-        {
-            return new instance_mechanar_InstanceMapScript(map);
-        }
+    public:
+        instance_mechanar(): InstanceMapScript("instance_mechanar", 554) { }
 
         struct instance_mechanar_InstanceMapScript : public InstanceScript
         {
             instance_mechanar_InstanceMapScript(Map* map) : InstanceScript(map)
             {
                 SetBossNumber(EncounterCount);
+                LoadDoorData(doorData);
             }
+            
 
-
-
-            uint32 GetData(uint32 type) const
+            void OnGameObjectCreate(GameObject* gameObject)
             {
-                switch (type)
+                switch (gameObject->GetEntry())
                 {
-                    case DATA_GATEWATCHER_GYROKILL:   return SetBossNumber[DATA_GATEWATCHER_GYROKILL];
-                    case DATA_IRON_HAND:   return SetBossNumber[DATA_IRON_HAND];
-                    case DATA_MECHANOLORD_CAPACITUS:   return SetBossNumber[DATA_MECHANOLORD_CAPACITUS];
-                    case DATA_NETHERMANCER_SEPRETHREA:   return SetBossNumber[DATA_NETHERMANCER_SEPRETHREA];
-                    case DATA_PATHALEON_THE_CALCULATOR:   return SetBossNumber[DATA_PATHALEON_THE_CALCULATOR];
+                    case GO_DOOR_MOARG_1:
+                    case GO_DOOR_MOARG_2:
+                    case GO_DOOR_NETHERMANCER:
+                        AddDoor(gameObject, true);
+                        break;
+                    default:
+                        break;
                 }
-
-                return false;
             }
 
-            uint64 GetData64(uint32 /*identifier*/) const
+            void OnGameObjectRemove(GameObject* gameObject)
             {
-                return 0;
+                switch (gameObject->GetEntry())
+                {
+                    case GO_DOOR_MOARG_1:
+                    case GO_DOOR_MOARG_2:
+                    case GO_DOOR_NETHERMANCER:
+                        AddDoor(gameObject, false);
+                        break;
+                    default:
+                        break;
+                }
             }
 
             bool SetBossState(uint32 type, EncounterState state)
@@ -72,7 +79,7 @@ class instance_mechanar : public InstanceMapScript
                 switch (type)
                 {
                     case DATA_GATEWATCHER_GYROKILL:
-                    case DATA_IRON_HAND:
+                    case DATA_GATEWATCHER_IRON_HAND:
                     case DATA_MECHANOLORD_CAPACITUS:
                     case DATA_NETHERMANCER_SEPRETHREA:
                     case DATA_PATHALEON_THE_CALCULATOR:
@@ -89,7 +96,7 @@ class instance_mechanar : public InstanceMapScript
                 OUT_SAVE_INST_DATA;
 
                 std::ostringstream saveStream;
-                saveStream << "S O " << GetBossSaveData();
+                saveStream << "M C " << GetBossSaveData();
 
                 OUT_SAVE_INST_DATA_COMPLETE;
                 return saveStream.str();
@@ -110,7 +117,7 @@ class instance_mechanar : public InstanceMapScript
                 std::istringstream loadStream(str);
                 loadStream >> dataHead1 >> dataHead2;
 
-                if (dataHead1 == 'S' && dataHead2 == 'O')
+                if (dataHead1 == 'M' && dataHead2 == 'C')
                 {
                     for (uint32 i = 0; i < EncounterCount; ++i)
                     {
@@ -127,10 +134,14 @@ class instance_mechanar : public InstanceMapScript
                 OUT_LOAD_INST_DATA_COMPLETE;
             }
         };
+
+        InstanceScript* GetInstanceScript(InstanceMap* map) const
+        {
+            return new instance_mechanar_InstanceMapScript(map);
+        }
 };
 
 void AddSC_instance_mechanar()
 {
-    new instance_mechanar;
+    new instance_mechanar();
 }
-
