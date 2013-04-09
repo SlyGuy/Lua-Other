609f56cfda8bc6b10244668297222d4e61356265
Ronvenal <zdroid9770@hotmail.com>
2013-04-09 13:48:47 -0500

    Update AllyMallGuard.lua

--- Funserver/ArcEmu/Lua/Alliance Custom stuff/AllyMallGuard.lua
+++ Funserver/ArcEmu/Lua/Alliance Custom stuff/AllyMallGuard.lua
@@ -1,43 +1,40 @@
---[[
+--[[ WoTD License - 
+This software is provided as free and open source by the
+team of The WoTD Team. This script was written and is
+protected by the GPL v2. Please give credit where credit
+is due, if modifying, redistributing and/or using this 
+software. Thank you.
+Thank: WoTD; for the Script
+~~End of License... Please Stand By...
+-- WoTD Team, Janurary 19, 2010. ]]
 
-	This is created by zdroid9770  :D
-
-	© Copyright 2012
-
-   Please if you share this elsewhere include me in the Credits for makeing this Script.
-   You can learn to make your own here:
-   "http://www.ac-web.org/forums/showthread.php?t=58843"
-   
-]]
-
-
+local ID = 10000 --Replace your ID here for your npc entry
 
 function Boss_OnCombat(pUnit, Event)
-pUnit:SendChatMessage(14, 0, "Another pesky Horde...")
-pUnit:RegisterEvent("Boss_Spell", 1000, 0)
+	pUnit:SendChatMessage(14, 0, "Another pesky Horde...")
+	pUnit:RegisterEvent("Boss_Spell", 1000, 0)
 end
 
 function Boss_Spell(pUnit, Event)
-pUnit:CastSpellOnTarget(5, pUnit:GetMainTank())
+	pUnit:CastSpellOnTarget(5, pUnit:GetMainTank())
 end
 
 function Boss_OnLeaveCombat(pUnit, Event)
-pUnit:SendChatMessage(14, 0, "ughhh")
-pUnit:RemoveEvents()
+	pUnit:SendChatMessage(13, 0, "You have faded...")
+	pUnit:RemoveEvents()
 end
 
 function Boss_OnKilledTarget(pUnit, Event)
-        pUnit:SendChatMessage(14, 0, "Don't come again or your banned by my GM skillz!")
-		pUnit:PlaySoundToSet(11466)
+        pUnit:SendChatMessage(14, 0, "You are not prepared!")
+	pUnit:PlaySoundToSet(11466)
         pUnit:CastSpell(36300)
 end
 
 function Boss_OnDeath(pUnit, Event)
-        pUnit:SendChatMessage(14, 0, "Oh Crap :(")
         pUnit:RemoveEvents()
 end
 
-RegisterUnitEvent(100000, 1, "Boss_OnCombat")
-RegisterUnitEvent(100000, 2, "Boss_OnLeaveCombat")
-RegisterUnitEvent(100000, 3, "Boss_OnKilledTarget")
-RegisterUnitEvent(100000, 4, "Boss_OnDeath")
+RegisterUnitEvent(ID, 1, "Boss_OnCombat")
+RegisterUnitEvent(ID, 2, "Boss_OnLeaveCombat")
+RegisterUnitEvent(ID, 3, "Boss_OnKilledTarget")
+RegisterUnitEvent(ID, 4, "Boss_OnDeath")
