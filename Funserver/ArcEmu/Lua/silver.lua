3c38916830ed6d15581e332e336fbf7856050980
Ronvenal <zdroid9770@hotmail.com>
2013-04-10 13:44:19 -0500

    Update silver.lua

--- Funserver/ArcEmu/Lua/silver.lua
+++ Funserver/ArcEmu/Lua/silver.lua
@@ -2,8 +2,8 @@ local MSG_REVIVE = "#silver"
 
 function SilverOnChat(event, player, message, type, language)
 	if(message == MSG_REVIVE) then
-			local GMrank = player:GetGmRank()
-			if (GMrank == 'vi') then
+		local GMrank = player:GetGmRank()
+		if (GMrank == 'vi') then
 			player:AddItem(28987, 1)
 			player:AddItem(28988, 1)
 			player:AddItem(28985, 1)
@@ -15,7 +15,7 @@ function SilverOnChat(event, player, message, type, language)
 			player:AddItem(28986, 1)
 			player:Emote(10)
 			player:SendBroadcastMessage("|cFF00FFFF[VIP System]|cFFFFA500|cFFFFA500Congratulations with your SILVER DONOR gear!")
-			else
+		else
 			player:SendBroadcastMessage("|cFF00FFFF[VIP System]|cFFFFA500|cFFFFA500You must be VIP to use this command, go donate on www.infinitygaming.org for VIP rank!!")
 			player:SendAreaTriggerMessage("|cFF00FFFF[VIP System]|cFFFFA500|cFFFFA500You must be VIP to use this command!")
 		end
