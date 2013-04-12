ad9671345c0255c698ae0d212265dd34fbef3efa
Ronvenal <zdroid9770@hotmail.com>
2013-04-12 10:24:27 -0500

    Update arenapoints.lua

--- Funserver/ArcEmu/Lua/Custom Commands/arenapoints.lua
+++ Funserver/ArcEmu/Lua/Custom Commands/arenapoints.lua
@@ -2,7 +2,7 @@
 
 	This is created by zdroid9770  :D
 
-	© Copyright 2012
+	ï¿½ Copyright 2012 - 2013
 
 ]]
 
@@ -10,17 +10,17 @@
 local MSG_HONOR = ".increase arena"
 
 function ArenaOnChat(event, player, message, type, language)
-	if(message == MSG_HONOR) then
+	if (message == MSG_HONOR) then
 			 if (player:GetArenaPoints() >= 50000) then
-			player:SendBroadcastMessage("|cFF00FFFF[WoD Message]|cFFFFA500|cFFFFA500Your arena points are at its max!")
+				player:SendBroadcastMessage("|cFF00FFFF[WoD Message]|cFFFFA500|cFFFFA500Your arena points are at its max!")
 			 else
 			    local GMrank = player:GetGmRank()
-			 if GMrank == "1"  or GMrank == "2" or GMrank == "3" or GMrank == "az" then
-			player:AddArenaPoints(2500)
-			player:SendBroadcastMessage("|cFF00FFFF[WoD Message]|cFFFFA500|cFFFFA500Added 2500 more arena points to your character.")
-			 else
-			player:SendBroadcastMessage("|cFF00FFFF[WoD Message]|cFFFFA500|cFFFFA500Your not a Staff member!")
-			player:SendAreaTriggerMessage("|cFF00FFFF[WoD Message]|cFFFFA500|cFFFFA500Not working!")
+			 if ((GMrank == "1")  or (GMrank == "2") or (GMrank == "3") or (GMrank == "az")) then
+				player:AddArenaPoints(2500)
+				player:SendBroadcastMessage("|cFF00FFFF[WoD Message]|cFFFFA500|cFFFFA500Added 2500 more arena points to your character.")
+			else
+				player:SendBroadcastMessage("|cFF00FFFF[WoD Message]|cFFFFA500|cFFFFA500Your not a Staff member!")
+				player:SendAreaTriggerMessage("|cFF00FFFF[WoD Message]|cFFFFA500|cFFFFA500Not working!")
 			end
 		end
 	end
